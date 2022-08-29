e2f1912b6e1eb85ecad1bfeb99ade7a28cbd16ea
Deionne <111102092+DDC76@users.noreply.github.com>
2022-08-28 21:32:04 -0500

    Add files via upload

---
+++ Week2-Homework.js
@@ -0,0 +1,20 @@
+const name = "D's Pizza Place";
+let numberOfToppings = 10;
+{
+  console.log(name);
+  console.log("Up to 10 Toppings");
+}
+
+let str = "D's Pizza Place offers up to 10 Toppings!";
+{
+  console.log(str); // D's Pizza Place offers up to 10 Toppings!
+}
+if (numberOfToppings <= 10);
+{
+  console.log("Quality, not quantity.");
+}
+for (let numberOfToppings = 1; numberOfToppings < 10; numberOfToppings += 2)
+  if (numberOfToppings > 10) break;
+{
+  console.log(numberOfToppings);
+}
