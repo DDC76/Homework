e463b3cae1328f9b1b63dbf81d403a79314858c3
Deionne <111102092+DDC76@users.noreply.github.com>
2022-08-31 23:25:47 -0500

    Update Week2-Homework.js

--- Week2-Homework.js
+++ Week2-Homework.js
@@ -1,20 +1,30 @@
 const name = "D's Pizza Place";
-let numberOfToppings = 10;
-{
-  console.log(name);
-  console.log("Up to 10 Toppings");
-}
+let numberOfToppings = 10;{
+console.log('"Dees Pizza Place"  "Up to 10 Toppings"');
+// "Dees Pizza Place"
+// "Up to 10 Toppings"
+  
+console.log(typeof "Pizza");
+  // expected output: "string"
+  
+console.log(typeof 10);
+  // expected output: "number"
 
-let str = "D's Pizza Place offers up to 10 Toppings!";
-{
+  console.log(typeof false);
+  // expected output: "boolean"
+  
   console.log(str); // D's Pizza Place offers up to 10 Toppings!
 }
 if (numberOfToppings <= 10);
 {
   console.log("Quality, not quantity.");
 }
+// expecte output: "Quality, not quantity if less than 10 toppings
+
 for (let numberOfToppings = 1; numberOfToppings < 10; numberOfToppings += 2)
   if (numberOfToppings > 10) break;
 {
   console.log(numberOfToppings);
 }
+
+// expecte output: 10
