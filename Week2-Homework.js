bb14d50e18a44d23148286ebf4406fedb59d8169
Deionne <111102092+DDC76@users.noreply.github.com>
2022-09-01 22:43:26 -0500

    Update Week2-Homework.js

--- Week2-Homework.js
+++ Week2-Homework.js
@@ -1,29 +1,27 @@
-const name = "Dees Pizza Place";
-let numberOfToppings = 10;{
-console.log('"Dees Pizza Place"  "Up to 10 Toppings"');
-// "Dees Pizza Place"
-// "Up to 10 Toppings"
-  
-console.log(typeof "Pizza");
-  // expected output: "string"
-  
-console.log(typeof 10);
-  // expected output: "number"
-
-  console.log(typeof false);
-  // expected output: "boolean"
-  
-  let str = "Dees Pizza Place offers up to 10 Toppings!";
-  console.log(str); // Dees Pizza Place offers up to 10 Toppings!
+// 4
+// inside your JS file, create two variables: pizzaPlace and numberOfToppings
+let pizzaPlace;
+let numberOfToppings;
+// 5
+// Have the variables reference the name of your Pizza Place and the number of toppings
+pizzaPlace = ("Dees Pizza Place");
+numberOfToppings = 20; {
+  console.log(pizzaPlace, typeof pizzaPlace);
+  console.log(numberOfToppings, typeof numberOfToppings);
+  console.log(`"Hello to ${pizzaPlace}, try any of our ${numberOfToppings} available toppings on our signature pizzas!"`);
 }
-if (numberOfToppings <= 10);
-{
+
+numberOfToppings = 12;
+
+if (numberOfToppings < 10) {
   console.log("Quality, not quantity.");
 }
+else { console.log("A Whole lot of Pizza!");
+}
+
 // expected output: "Quality, not quantity if less than 10 toppings
 for (let numberOfToppings = 1; numberOfToppings < 10; numberOfToppings += 2)
-  if (numberOfToppings > 10) break;
-{
+  if (numberOfToppings > 10) break; {
   console.log(numberOfToppings);
 }
 // expected output: 10
