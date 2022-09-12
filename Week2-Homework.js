59f5aeb2d5657422ad759f6ee638d9602923756d
Deionne <111102092+DDC76@users.noreply.github.com>
2022-09-12 10:52:42 -0500

    Week2-Homework

--- Week2-Homework.js
+++ Week2-Homework.js
@@ -1,36 +1,22 @@
-// 4
 // inside your JS file, create two variables: pizzaPlace and numberOfToppings
-let pizzaPlace;
-let numberOfToppings;
-// 5
 // Have the variables reference the name of your Pizza Place and the number of toppings
-pizzaPlace = ("Dees Pizza Place");
-numberOfToppings = 20; {
-  console.log(pizzaPlace, typeof pizzaPlace);
-  console.log(numberOfToppings, typeof numberOfToppings);
-  console.log(`"Hello to ${pizzaPlace}, try any of our ${numberOfToppings} available toppings on our signature pizzas!"`);
-}
+let pizzaPlace = "Dees Pizza Place";
+let numberOfToppings = 20;
 
-numberOfToppings = 12;
+// print the types of two variables that reference two different data types (typeof)
+console.log(typeof pizzaPlace, typeof numberOfToppings);
+// string
+// number
+let pizzaInfo = `Welcome to ${pizzaPlace}, try any of our ${numberOfToppings} available toppings on our signature pizzas!`;
+
+console.log(pizzaInfo);
+
+//let numberOfToppings = 20;
 
 if (numberOfToppings < 10) {
   console.log("Quality, not quantity.");
-}
-else { console.log("A Whole lot of Pizza!");
+} else {
+  console.log("A Whole lot of Pizza!");
 }
 
 // expected output: "Quality, not quantity if less than 10 toppings
-for (let numberOfToppings = 1; numberOfToppings < 10; numberOfToppings += 2)
-  if (numberOfToppings > 10) break; {
-  console.log(numberOfToppings);
-}
-// expected output: 10
-
-// Loop
-let x = numberOfToppings;
-  if (x = 1,  x <= 10,  x++) {
-    console.log(numberOfToppings);
-  }
-  else {(x % 2 === 10); {
-console.log(x);
-}
